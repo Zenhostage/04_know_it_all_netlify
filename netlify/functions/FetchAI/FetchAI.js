dcd368dd0875ae4ad85ca79f6fcd504016b5e283
zenhostage <cristiananghel84@gmail.com>
2023-11-29 13:44:38 +0200

    wi <id>: <type>: <summary>
    <description>

--- netlify/functions/FetchAI/FetchAI.js
+++ netlify/functions/FetchAI/FetchAI.js
@@ -1,34 +1,38 @@
-import { OpenAI } from 'openai';
+const { OpenAI } = require('openai');
 
 const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY
-})
-console.log('before handler')
-const handler = async (event) => {
-  const conversationHistory  = event.body.message; // Extract message from request body
-  console.log('conv is ' + conversationHistory)
+});
+
+console.log('before handler');
+
+exports.handler = async (event) => {
+  if (event.httpMethod !== 'POST') {
+    return { statusCode: 405, body: 'Method Not Allowed' };
+  }
+
   try {
+    const body = JSON.parse(event.body);
+    const conversationHistory = body.message;
+
+    console.log('conv is ' + JSON.stringify(conversationHistory));
+
     const response = await openai.chat.completions.create({
       model: 'gpt-4',
       messages: conversationHistory,
-       max_tokens: 50,
-       presence_penalty:0,
-       frequency_penalty:0.3
-  }) 
-
+      max_tokens: 50,
+      presence_penalty: 0,
+      frequency_penalty: 0.3
+    });
 
     return {
       statusCode: 200,
-      body: JSON.stringify({ 
-        
-        reply: response.choices[0].message.content 
-    
-    }),
-      
-    }
+      body: JSON.stringify({
+        reply: response.choices[0].message.content
+      }),
+    };
   } catch (error) {
-    return { statusCode: 500, body: error.toString() }
+    console.error(error);
+    return { statusCode: 500, body: JSON.stringify({ error: error.message }) };
   }
-}
-
-module.exports = { handler }
+};
